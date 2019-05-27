b5753200b69da483a69e697cabfacaae5200cd1e
Kostas Aronis <k.aronis@dotbydot.gr>
2019-05-27 16:55:35 +0300

    corrected silly copy paste mistake

--- src/Panacea.Modularity.RemoteDesktop/PanaceaServicesExtensions.cs
+++ src/Panacea.Modularity.RemoteDesktop/PanaceaServicesExtensions.cs
@@ -5,12 +5,12 @@ namespace Panacea.Modularity.RemoteDesktop
 {
     public static class PanaceaServicesExtensions
     {
-        public static IRemoteDesktop GetImprivata(this PanaceaServices core)
+        public static IRemoteDesktop GetRemoteDesktopPlugin(this PanaceaServices core)
         {
             return core.PluginLoader.GetPlugin<IRemoteDesktop>();
         }
 
-        public static bool TryGetCitrix(this PanaceaServices core, out IRemoteDesktop plugin)
+        public static bool TryGetGetRemoteDesktopPlugin(this PanaceaServices core, out IRemoteDesktop plugin)
         {
             plugin = null;
             var rdc = core.PluginLoader.GetPlugins<IRemoteDesktop>().FirstOrDefault();
