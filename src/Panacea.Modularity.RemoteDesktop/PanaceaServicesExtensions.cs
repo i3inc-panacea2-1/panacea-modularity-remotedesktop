aba685dbc2a46f26b5228dcdd94473481507c075
Kostas Aronis <k.aronis@dotbydot.gr>
2019-05-27 15:27:23 +0300

    Empty interface, scaffolding

---
+++ src/Panacea.Modularity.RemoteDesktop/PanaceaServicesExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Panacea.Core;
+using System.Linq;
+
+namespace Panacea.Modularity.RemoteDesktop
+{
+    public static class PanaceaServicesExtensions
+    {
+        public static IRemoteDesktop GetImprivata(this PanaceaServices core)
+        {
+            return core.PluginLoader.GetPlugin<IRemoteDesktop>();
+        }
+
+        public static bool TryGetCitrix(this PanaceaServices core, out IRemoteDesktop plugin)
+        {
+            plugin = null;
+            var rdc = core.PluginLoader.GetPlugins<IRemoteDesktop>().FirstOrDefault();
+            if (rdc == null)
+            {
+                return false;
+            }
+            plugin = rdc;
+            return true;
+        }
+    }
+}
